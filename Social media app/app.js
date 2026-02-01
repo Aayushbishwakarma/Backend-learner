9da5e8c5e89c8e0d850c3436f19807d2ad5bb575
Aayushbishwakarma <aayushb.k2060@gmail.com>
2026-02-01 20:44:17 +0545

    profile section done

---
+++ Social media app/app.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express();
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+app.set('view engine', 'ejs'); 
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('token',"");
+    res.redirect('/login');
+});
+
+app.get('/profile',isLoggedIn, async (req, res) => {
+    let user =await userModel.findOne({email: req.user.email}).populate('posts');
+    res.render('profile', {user});
+});
+
+
+
+app.post('/post',isLoggedIn, async (req, res) => {
+    let user =await userModel.findOne({email: req.user.email})
+    let post = await postModel.create({
+        user: user._id,
+        content: req.body.content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect('/profile');
+});
+
+app.post('/register', async(req, res) => {
+    let{email, username, name , password, age} = req.body ;
+
+    let user = await userModel.findOne({email})
+    if(user) return res.status(500).send('User already exists');
+
+    bcrypt.genSalt(10, (err, salt) =>{
+        bcrypt.hash(password, salt, async(err, hash) =>{
+           let user = await userModel.create({
+                email,
+                username,
+                name,
+                password: hash,
+                age
+            });
+
+
+            let token = jwt.sign({email: email, userid: user._id}, "sshhh");
+            res.cookie('token', token);
+            res.send("registered");
+        })
+    })
+});
+
+
+app.post('/login', async(req, res) => {
+    let{email, password} = req.body ;
+
+    let user = await userModel.findOne({email})
+    if(!user) return res.status(500).send('Something went wrong');
+
+    bcrypt.compare(password, user.password, function (err,result) {
+        if(result) {
+            let token = jwt.sign({email: email, userid: user._id}, "sshhh");
+            res.cookie('token', token);
+            res.status(200).redirect('/profile');     
+        }
+        else res.redirect("/login");
+    })
+});
+
+
+function isLoggedIn(req, res, next) {
+   if(req.cookies.token === "") res.redirect("/login");
+   else{
+    let data = jwt.verify(req.cookies.token, "sshhh");
+    req.user = data;
+   
+   next();
+
+   }
+
+}
+
+
+
+app.listen(3000);
---
+++ Social media app/read.md
@@ -0,0 +1,3 @@
+user can login and logout
+user can post and like the post
+user can delete the post
