f4ca2c67534bf5eab8304d2ec783f431e8302f91
Aayushbishwakarma <kushb.k123@gmail.com>
2026-05-27 07:50:41 +0545

    create route created

--- Ecommerce Project/routes/ownersRouter.js
+++ Ecommerce Project/routes/ownersRouter.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
-
+const ownerModel = require("../models/owners-model");
 
 
 router.get("/", (req, res) => {
     res.send("hey");
 });
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async function(req, res)  {
+        let owners = await ownerModel.find();
+        if(owners.length > 0){ 
+            return res
+                .status(502)
+                .send("Owners already exist")
+        }
+
+
+        let createdOwner = {fullname, email, password} =req.body;
+        
+        await ownerModel.create({
+                fullname,
+                email,
+                password
+        });
+        res.status(502).send(createdOwner);
+    });
+}
 
 module.exports = router;
